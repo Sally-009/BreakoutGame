e14b5a6f13e7dad371d9f1ffb58ee9ec0d400a35
Saori Kojima <skojima@atu.edu>
2024-02-26 16:42:33 -0600

    initial commit

---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Breakout_Game
+{
+    public partial class Form1 : Form
+    {
+
+        bool goLeft;
+        bool goRight;
+        bool isGameOver;
+
+        int score;
+        int ballx;
+        int bally;
+        int playerSpeed;
+
+        // Random number
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            setupGame();
+        }
+
+        private void setupGame()
+        {
+            // Initialize
+            score = 0;
+            ballx = 5;
+            bally = 5;
+            playerSpeed = 12;
+            txtScore.Text = "Score: " + score;
+
+            gameTimer.Start();
+
+            // Change the color of blocks randomly
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox && (string)x.Tag == "blocks")
+                {
+                    x.BackColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+                }
+            }
+        }
+
+        private void mainGameTimerEvent(object sender, EventArgs e)
+        {
+            // move to left until it reaches to the edge of screen
+            if (goLeft == true && player.Left > 0)
+                player.Left -= playerSpeed;
+
+            // move to right until it reaches to the edge of screen
+            if (goRight == true && player.Left < 700)
+                player.Left += playerSpeed;
+
+        }
+
+        // keep moving while the key is pressed
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+                goLeft = true;
+            if(e.KeyCode == Keys.Right)
+                goRight = true;
+        }
+
+        // stop moving when the key is released
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+                goLeft = false;
+            if (e.KeyCode == Keys.Right)
+                goRight = false;
+        }
+    }
+}
